bf03652018a2fbac2675a5876872dfc0f1a53cc4
rurigrass <ruripg@gmail.com>
2024-05-23 22:06:22 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useRef } from "react";
+import "./App.css";
+
+function App() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    const video = videoRef.current;
+    if (!video) return;
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+    drawAmbient(ctx, canvas, video);
+  }, []);
+
+  const drawAmbient = (
+    ctx: CanvasRenderingContext2D,
+    canvas: HTMLCanvasElement,
+    video: HTMLVideoElement
+  ) => {
+    video.ontimeupdate = () => {
+      ctx.drawImage(video, 0, 0, 1, 1, 0, 0, canvas.width, canvas.height);
+    };
+  };
+
+  return (
+    <div className="page">
+      <div className="container">
+        <canvas className="canvas" ref={canvasRef}></canvas>
+        <video
+          ref={videoRef}
+          controls
+          autoPlay
+          muted
+          loop
+          src="/media/AmbientModeTest.mp4"
+        ></video>
+      </div>
+    </div>
+  );
+}
+
+export default App;
